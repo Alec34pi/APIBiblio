c292af74890f979263beafaadb66ead585431545
Alec34pi <alec.pibre@gmail.com>
2025-11-02 19:10:48 +0100

    t

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ let browser = null;
 
 async function startBrowser() {
   if (!browser) {
+    const executablePath = await chromium.executablePath(); // correction ici
     browser = await puppeteer.launch({
-      executablePath: chromium.path, // chemin correct fourni par le package
+      executablePath,
       headless: true,
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
@@ -32,6 +33,7 @@ async function getPriceFromMomox(barcode) {
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     );
+
     await page.setExtraHTTPHeaders({
       Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
       "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -40,6 +42,7 @@ async function getPriceFromMomox(barcode) {
 
     await page.goto(`https://www.momox.fr/offer/${barcode}`, { waitUntil: "networkidle2" });
 
+    // Sélecteur pour le prix
     const priceText = await page.$eval(
       "div.searchresult-price.text-center.text-xxl.font-medium",
       el => el.textContent.trim()
